15226c781c9e6a504524ea38f88e809b540a65f8
Shejal854 <146625199+Shejal854@users.noreply.github.com>
2026-04-02 17:39:09 +0530

    add missing social media URLs in footer

--- src/constants/index.js
+++ src/constants/index.js
@@ -22,14 +22,17 @@ const socialImgs = [
   {
     name: "LinkedIn",
     imgPath: "/images/logos/linkedin.svg",
+    url: "https://www.linkedin.com/in/shejal854/"
   },
   {
     name: "Github",
     imgPath: "/images/logos/github.png",
+    url: "https://github.com/Shejal854"
   },
   {
     name: "LeetCode",
     imgPath: "/images/logos/leetcode.png",
+    url: "https://leetcode.com/u/shejal12/"
   },
 ];
 
